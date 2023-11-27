8b127fee4405106b294dbbbb4bacb55fb5526069
MunavarPM <munavarmunna101@gmail.com>
2023-11-27 14:00:05 +0530

    Initial Commit

---
+++ AgendaUsingJSON/AgendaUsingJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgendaUsingJSONApp.swift
+//  AgendaUsingJSON
+//
+//  Created by MUNAVAR PM on 27/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AgendaUsingJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
